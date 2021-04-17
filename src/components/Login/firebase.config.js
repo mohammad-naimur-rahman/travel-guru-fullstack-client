edd2a315a84de750a223d80bfdcd57e4e90a4923
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2021-04-18 04:36:01 +0600

    Assignment 11 done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBFfOrRAZEt_b1KBca8BHnGEw9g-yaVip4",
+    authDomain: "travel-guru-e1c27.firebaseapp.com",
+    projectId: "travel-guru-e1c27",
+    storageBucket: "travel-guru-e1c27.appspot.com",
+    messagingSenderId: "582456795581",
+    appId: "1:582456795581:web:cb328367d2dcf822d6f2d4"
+};
